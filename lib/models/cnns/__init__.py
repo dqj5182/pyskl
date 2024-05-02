74a7372a0e5347038c6b65a339e0116498a19aaa
dqj5182 <dqj5182@snu.ac.kr>
2024-05-02 17:04:06 +0000

    update

--- lib/models/cnns/__init__.py
+++ lib/models/cnns/__init__.py
@@ -1,8 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .resnet import ResNet
 from .resnet3d_slowonly import ResNet3dSlowOnly
-from .x3d import X3D
 
 __all__ = [
-    'X3D', 'ResNet', 'ResNet3dSlowOnly'
+    'ResNet3dSlowOnly'
 ]
