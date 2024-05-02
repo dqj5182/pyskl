fa58a1dd580625e894956fe55e11e6b657cf9def
dqj5182 <dqj5182@snu.ac.kr>
2024-05-02 12:14:46 +0000

    update

--- tools/test.py
+++ tools/test.py
@@ -4,21 +4,16 @@ import argparse
 import mmcv
 import os
 import os.path as osp
-import time
-import torch
 import torch.distributed as dist
 from mmcv import Config
-from mmcv import digit_version as dv
-from mmcv import load
-from mmcv.cnn import fuse_conv_bn
 from mmcv.engine import multi_gpu_test
 from mmcv.fileio.io import file_handlers
-from mmcv.parallel import MMDistributedDataParallel
+from mmcv.parallel import MMDataParallel
 from mmcv.runner import get_dist_info, init_dist, load_checkpoint
 
 from pyskl.datasets import build_dataloader, build_dataset
 from pyskl.models import build_model
-from pyskl.utils import cache_checkpoint, mc_off, mc_on, test_port
+from pyskl.utils import cache_checkpoint
 
 
 def parse_args():
@@ -64,44 +59,19 @@ def parse_args():
     args = parser.parse_args()
     if 'LOCAL_RANK' not in os.environ:
         os.environ['LOCAL_RANK'] = str(args.local_rank)
-
     return args
 
 
 def inference_pytorch(args, cfg, data_loader):
     """Get predictions by pytorch models."""
-    if args.average_clips is not None:
-        # You can set average_clips during testing, it will override the
-        # original setting
-        if cfg.model.get('test_cfg') is None and cfg.get('test_cfg') is None:
-            cfg.model.setdefault('test_cfg',
-                                 dict(average_clips=args.average_clips))
-        else:
-            if cfg.model.get('test_cfg') is not None:
-                cfg.model.test_cfg.average_clips = args.average_clips
-            else:
-                cfg.test_cfg.average_clips = args.average_clips
 
     # build the model and load checkpoint
     model = build_model(cfg.model)
-    if dv(torch.__version__) >= dv('2.0.0') and args.compile:
-        model = torch.compile(model)
-
-    if args.checkpoint is None:
-        work_dir = cfg.work_dir
-        args.checkpoint = osp.join(work_dir, 'latest.pth')
-        assert osp.exists(args.checkpoint)
 
     args.checkpoint = cache_checkpoint(args.checkpoint)
     load_checkpoint(model, args.checkpoint, map_location='cpu')
 
-    if args.fuse_conv_bn:
-        model = fuse_conv_bn(model)
-
-    model = MMDistributedDataParallel(
-        model.cuda(),
-        device_ids=[torch.cuda.current_device()],
-        broadcast_buffers=False)
+    model = MMDataParallel(model.cuda(cfg.gpu_ids[0]), device_ids=cfg.gpu_ids)
     outputs = multi_gpu_test(model, data_loader, args.tmpdir)
 
     return outputs
@@ -126,9 +96,6 @@ def main():
     _, suffix = osp.splitext(out)
     assert suffix[1:] in file_handlers, ('The format of the output file should be json, pickle or yaml')
 
-    # set cudnn benchmark
-    if cfg.get('cudnn_benchmark', False):
-        torch.backends.cudnn.benchmark = True
     cfg.data.test.test_mode = True
 
     if not hasattr(cfg, 'dist_params'):
@@ -147,37 +114,17 @@ def main():
     dataloader_setting = dict(dataloader_setting, **cfg.data.get('test_dataloader', {}))
     data_loader = build_dataloader(dataset, **dataloader_setting)
 
-    default_mc_cfg = ('localhost', 22077)
-    memcached = cfg.get('memcached', False)
-
-    if rank == 0 and memcached:
-        # mc_list is a list of pickle files you want to cache in memory.
-        # Basically, each pickle file is a dictionary.
-        mc_cfg = cfg.get('mc_cfg', default_mc_cfg)
-        assert isinstance(mc_cfg, tuple) and mc_cfg[0] == 'localhost'
-        if not test_port(mc_cfg[0], mc_cfg[1]):
-            mc_on(port=mc_cfg[1], launcher=args.launcher)
-        retry = 3
-        while not test_port(mc_cfg[0], mc_cfg[1]) and retry > 0:
-            time.sleep(5)
-            retry -= 1
-        assert retry >= 0, 'Failed to launch memcached. '
-
     dist.barrier()
     outputs = inference_pytorch(args, cfg, data_loader)
 
-    rank, _ = get_dist_info()
-    if rank == 0:
-        print(f'\nwriting results to {out}')
-        dataset.dump_results(outputs, out=out)
-        if eval_cfg:
-            eval_res = dataset.evaluate(outputs, **eval_cfg)
-            for name, val in eval_res.items():
-                print(f'{name}: {val:.04f}')
+    print(f'\nwriting results to {out}')
+    dataset.dump_results(outputs, out=out)
+    if eval_cfg:
+        eval_res = dataset.evaluate(outputs, **eval_cfg)
+        for name, val in eval_res.items():
+            print(f'{name}: {val:.04f}')
 
     dist.barrier()
-    if rank == 0 and memcached:
-        mc_off()
 
 
 if __name__ == '__main__':
