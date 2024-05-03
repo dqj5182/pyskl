c672ee5e11f953a5c6a13db963cf1ff9a8e94646
dqj5182 <dqj5182@snu.ac.kr>
2024-05-03 01:20:13 +0000

    update

--- lib/apis/__init__.py
+++ lib/apis/__init__.py
@@ -1,10 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from mmcv.engine import multi_gpu_test, single_gpu_test
 
-from .inference import inference_recognizer, init_recognizer
 from .train import train_model
 
 __all__ = [
-    'train_model', 'init_recognizer', 'inference_recognizer', 'multi_gpu_test',
+    'train_model', 'multi_gpu_test',
     'single_gpu_test'
 ]
